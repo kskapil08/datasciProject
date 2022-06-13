87634246e25758a851c697bfa74c6f560facbfdd
kskapil08 <88305206+kskapil08@users.noreply.github.com>
2022-06-13 21:06:44 +0530

    Update color_detection.py

--- color_detection.py
+++ color_detection.py
@@ -3,24 +3,24 @@ import numpy as np
 import pandas as pd
 import argparse
 
-#Creating argument parser to take image path from command line
+
 ap = argparse.ArgumentParser()
 ap.add_argument('-i', '--image', required=True, help="Image Path")
 args = vars(ap.parse_args())
 img_path = args['image']
 
-#Reading the image with opencv
+
 img = cv2.imread(img_path)
 
-#declaring global variables (are used later on)
+
 clicked = False
 r = g = b = xpos = ypos = 0
 
-#Reading csv file with pandas and giving names to each column
+
 index=["color","color_name","hex","R","G","B"]
 csv = pd.read_csv('colors.csv', names=index, header=None)
 
-#function to calculate minimum distance from all colors and get the most matching color
+
 def getColorName(R,G,B):
     minimum = 10000
     for i in range(len(csv)):
@@ -30,7 +30,7 @@ def getColorName(R,G,B):
             cname = csv.loc[i,"color_name"]
     return cname
 
-#function to get x,y coordinates of mouse double click
+
 def draw_function(event, x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDBLCLK:
         global b,g,r,xpos,ypos, clicked
@@ -50,10 +50,10 @@ while(1):
     cv2.imshow("image",img)
     if (clicked):
    
-        #cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle 
+        
         cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
 
-        #Creating text string to display( Color name and RGB values )
+        
         text = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
         
         #cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
